0ba8f230be745f8039c2e241c30bf269ee062124
Alex Charlton <acharlton@foko.co>
2015-07-28 20:04:31 -0400

    Fix defines

--- qrencode/qrencode.h
+++ qrencode/qrencode.h
@@ -18,6 +18,12 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#define __STATIC static
+#define MAJOR_VERSION 3
+#define MINOR_VERSION 9
+#define MICRO_VERSION 0
+#define VERSION "3.9.0"
+
 /** \mainpage
  * Libqrencode is a library for encoding data in a QR Code symbol, a kind of 2D
  * symbology.
